3aa7a530243dc52810a39c90fdb96941bf5ab021
MD Irfan <irfan02md@gmail.com>
2023-08-19 16:03:49 +0530

    Commiting scretch project of whatsup clone

---
+++ src/main/java/com/mdtech/whatsupbackendclone/WhatsupBackendCloneApplication.java
@@ -0,0 +1,13 @@
+package com.mdtech.whatsupbackendclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsupBackendCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsupBackendCloneApplication.class, args);
+	}
+
+}
